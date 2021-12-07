bc6ff9f466eb646da1cdff61a8b6c24a191c9ab7
WebDev-Chad <chadwin.allotey@gmail.com>
2021-12-06 20:54:07 -0500

    Update TextScreen.js

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,21 +1,24 @@
-import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  TextInput,
-} from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
+
+const [password,setPassword] = useState('');
+
   return (
     <View>
+        <Text>Enter Password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        value={password}
+        onChangeText={(newValue)=> setPassword(newValue)}
       />
+       {password.length < 5 && <Text>Password must be longer than 5 characters</Text>}
     </View>
+
+   
   );
 };
 
